f3c7f8d070e77bf78c687f9588f48a6167cdec06
AbdoXBuilder <myitworkingacc@gmail.com>
2025-09-30 12:02:41 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,7 +8,7 @@ repo init -u https://github.com/RisingOS-Revived/android -b sixteen --git-lfs
 
 # Device Tree
 git clone https://github.com/AbdoXBuilder/device_xiaomi_miatoll.git -b 166 device/xiaomi/miatoll
-git clone https://github.com/AbdoXBuilder/device_xiaomi_sm6250-common.git -b 16 device/xiaomi/sm6250-common
+git clone https://github.com/AbdoXBuilder/device_xiaomi_sm6250-common.git -b los device/xiaomi/sm6250-common
 
 #Device Vendor
 git clone https://github.com/AbdoXBuilder/vendor_xiaomi_miatoll.git -b 16 vendor/xiaomi/miatoll
@@ -21,9 +21,6 @@ git clone https://github.com/AbdoXBuilder/kernel_xiaomi_sm6250.git -b 16 kernel/
 #Device Hardware 
 git clone https://github.com/AbdoXBuilder/hardware_xiaomi.git -b 16-dolby hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_sony_timekeep.git -b lineage-22.2 hardware/sony/timekeep
-
-# Updates
-git clone https://github.com/AbdoAhmedElbanaa/android_packages_apps_Updater.git -b sixteen packages/apps/Updater
 
 # Sync
 /opt/crave/resync.sh
